d7275bd00e8ce0b23cc7f8fd6e27d6186e2be9b7
Pramiti Muppidi <124477650+Pramitim@users.noreply.github.com>
2025-10-18 21:41:48 -0400

    made sure linters and formatters pass

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -9,7 +9,6 @@ import cv2
 import numpy as np
 
 
-
 class DetectBlue:
     """
     Detects blue objects from an image.
@@ -49,14 +48,11 @@ class DetectBlue:
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Set upper and lower bounds for colour detection, this is in HSV
-        lower_blue = np.array([100,150,0])
-        upper_blue = np.array([150,225,255])
+        lower_blue = np.array([100, 150, 0])
+        upper_blue = np.array([150, 225, 255])
 
         # Apply the threshold for the colour detection
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-        # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img,img, mask= mask)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -70,7 +66,7 @@ class DetectBlue:
         cv2.imwrite(str(output_path), img)
 
         # Show res to see the result of what is being filtered in the colour detection
-        #cv2.imwrite(str(output_path), res)
+        # cv2.imwrite(str(output_path), res)
 
         # This parameter is needed to run tests
         return mask if return_mask else None
@@ -115,19 +111,16 @@ class DetectRed:
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Set upper and lower bounds for colour detection, this is in HSV
-        lower_red = np.array([0,50,50])
-        upper_red = np.array([10,255,255])
+        lower_red = np.array([0, 50, 50])
+        upper_red = np.array([10, 255, 255])
 
-        lower_red2 = np.array([150,100,100])
-        upper_red2 = np.array([180,255,255])
+        lower_red2 = np.array([150, 100, 100])
+        upper_red2 = np.array([180, 255, 255])
 
         # Apply the threshold for the colour detection
         mask1 = cv2.inRange(hsv, lower_red, upper_red)
         mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
         mask = cv2.bitwise_or(mask1, mask2)
-
-        # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img,img, mask= mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable
